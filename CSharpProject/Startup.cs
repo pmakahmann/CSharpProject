10bd7822e039f5482cafc09371de6bacbd5fcb6d
pmakahmann <pmakahmann@hotmail.com>
2017-05-31 17:37:15 -0400

    Add project files.

---
+++ CSharpProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSharpProject.Startup))]
+namespace CSharpProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
