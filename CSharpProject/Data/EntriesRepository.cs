49cacd91637a285e4b4681fb6780ed4f649732fc
pmakahmann <pmakahmann@hotmail.com>
2017-07-04 20:09:26 -0400

    Created and updated EntriesRepository.cs for non-persisiting data interaction with controller and views.

---
+++ CSharpProject/Data/EntriesRepository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CSharpProject.Models;
+
+namespace CSharpProject.Data
+{
+    public class EntriesRepository
+    {
+        /// <summary>
+        /// Returns a collection of entries.
+        /// </summary>
+        /// <returns>A list of entries.</returns>
+        public List<Entry> GetEntries()
+        {
+            return Data.Entries
+                .OrderByDescending(e => e.Name)
+                .ToList();
+        }
+
+        /// <summary>
+        /// Returns a single entry for the provided ID.
+        /// </summary>
+        /// <param name="id">The ID for the entry to return.</param>
+        /// <returns>An entry.</returns>
+        public Entry GetEntry(int id)
+        {
+            Entry entry = Data.Entries
+                .Where(e => e.Id == id)
+                .SingleOrDefault();
+
+            return entry;
+        }
+
+        /// <summary>
+        /// Adds an entry.
+        /// </summary>
+        /// <param name="entry">The entry to add.</param>
+        public void AddEntry(Entry entry)
+        {
+            // Get the next available entry ID.
+            int nextAvailableEntryId = Data.Entries
+                .Max(e => e.Id) + 1;
+
+            entry.Id = nextAvailableEntryId;
+
+            Data.Entries.Add(entry);
+        }
+
+        /// <summary>
+        /// Updates an entry.
+        /// </summary>
+        /// <param name="entry">The entry to update.</param>
+        public void UpdateEntry(Entry entry)
+        {
+            // Find the index of the entry that we need to update.
+            int entryIndex = Data.Entries.FindIndex(e => e.Id == entry.Id);
+
+            if (entryIndex == -1)
+            {
+                throw new Exception(
+                    string.Format("Unable to find an entry with an ID of {0}", entry.Id));
+            }
+
+            Data.Entries[entryIndex] = entry;
+        }
+
+        /// <summary>
+        /// Deletes an entry.
+        /// </summary>
+        /// <param name="id">The ID of the entry to delete.</param>
+        public void DeleteEntry(int id)
+        {
+            // Find the index of the entry that we need to delete.
+            int entryIndex = Data.Entries.FindIndex(e => e.Id == id);
+
+            if (entryIndex == -1)
+            {
+                throw new Exception(
+                    string.Format("Unable to find an entry with an ID of {0}", id));
+            }
+
+            Data.Entries.RemoveAt(entryIndex);
+        }
+    }
+}
